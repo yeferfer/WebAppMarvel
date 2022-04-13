ebc2d04684348deb0b17f9ee85a0db0ecfd23837
al-prieto-treinta <alejandro.prieto@treinta.co>
2022-04-13 12:11:46 -0500

    Slider featured comics

--- scripts/home.js
+++ scripts/home.js
@@ -217,3 +217,43 @@ const GetCardsData = () => {
 };
 
 GetCardsData();
+
+/* Carrousel Cards featured Comics Start */
+
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+/* Carrousel Cards featured Comics End */
